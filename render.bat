dba1db1806d9f64543534c83b241512571cd6e17
kk <smilebowl-trump27@yahoo.co.jp>
2020-05-19 22:19:27 +0900

    add worldwide

--- render.bat
+++ render.bat
@@ -1,8 +1,14 @@
 rem @echo off
 curl -OL https://dl.dropboxusercontent.com/s/6mztoeb6xf78g5w/COVID-19.csv
+
+curl -OL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+curl -OL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+curl -OL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
+
 setlocal enabledelayedexpansion
 cd /d %~dp0
-set rpath=C:\Program Files\R\R-3.6.2\bin
+rem set rpath=C:\Program Files\R\R-3.6.2\bin
+set rpath=C:\Program Files\R\R-4.0.0\bin
 set logile=render.log
 
 set inputFile=index.Rmd
